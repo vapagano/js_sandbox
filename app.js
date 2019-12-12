daf8da38ae70a26427710425d25fd9eb68122179
Víctor Ariel Pagano <victor.pagano@cultura.gob.ar>
2019-12-12 15:44:16 -0300

    Primer Commit

---
+++ app.js
@@ -0,0 +1,6 @@
+document.querySelector('.clear-tasks').addEventListener('click', onClick);
+
+function onClick(e) {
+    e.preventDefault();
+    alert('clicked');
+}
---
+++ tasklist/app.js
@@ -0,0 +1,148 @@
+// Define UI Vars
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+// Call load all event listeners
+loadEventListeners();
+
+// Load all event listeners
+function loadEventListeners() {
+    // DOM load event
+    document.addEventListener('DOMContentLoaded', getTasks);
+    //Add task event
+    form.addEventListener('submit', addTask);
+    // Remove task event
+    taskList.addEventListener('click', removeTask);
+    // Clear task event
+    clearBtn.addEventListener('click', clearTasks);
+    // Filter tasks event
+    filter.addEventListener('keyup', filterTask);
+}
+
+// Get tasks from LS
+function getTasks() {
+    let tasks = checkLocalStorage();
+
+    tasks.forEach(function(task){
+        addTaskItem(task);
+    });
+}
+
+// Check LS registers
+function checkLocalStorage() {
+    let tasks;
+    if (localStorage.getItem('tasks') === null) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    return tasks;
+}
+
+// Add Task
+function addTask(e) {
+
+    if (taskInput.value === '') {
+        alert('Add a task');
+    }
+
+    addTaskItem(taskInput.value);
+
+    // Store in Local Storage
+    storeTaskInLocalStorage(taskInput.value);
+
+    // Clear input
+    taskInput.value = '';
+    
+    e.preventDefault();
+}
+
+// Add Task item
+function addTaskItem(task) {
+     // Create li element
+     const li = document.createElement('li');
+     // Add class
+     li.className = 'collection-item';
+     // Create text node and append to li
+     li.appendChild(document.createTextNode(task));
+     // Create new link element 
+     const link = document.createElement('a');
+     // Add class
+     link.className = 'delete-item secondary-content';
+     // Add icon html
+     link.innerHTML = '<i class="fa fa-remove"></i>';
+     // Append the link to li
+     li.appendChild(link);
+     // Append li to ul
+     taskList.appendChild(li); 
+}
+
+// Store Task
+function storeTaskInLocalStorage(task) {
+    let tasks = checkLocalStorage();
+
+    tasks.push(task);
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+// Remove Task
+function removeTask(e) {
+    if (e.target.parentElement.classList.contains('delete-item')) {
+        if (confirm('Are you sure?')) {
+            e.target.parentElement.parentElement.remove();
+
+            // Remove from LS
+            removeTaskFromLocalStorage(e.target.parentElement.parentElement);
+        }
+    }
+    
+}
+
+// Remove from LS
+function removeTaskFromLocalStorage(taskItem) {
+    let tasks = checkLocalStorage();
+
+    tasks.forEach(function(task, index){
+        if (taskItem.textContent === task) {
+            tasks.splice(index, 1);
+        }
+    });
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+// Clear Tasks
+function clearTasks() {
+
+    while(taskList.firstChild){
+        taskList.removeChild(taskList.firstChild);
+    }
+
+    // Clear from LS
+    clearTasksFromLocalStorage();
+}
+
+// Clear Tasks from LS
+function clearTasksFromLocalStorage() {
+    localStorage.clear();
+}
+
+// Filter Tasks
+function filterTask(e) {
+    const text = e.target.value.toLowerCase();
+
+    document.querySelectorAll('.collection-item').forEach(
+        function(task){
+            const item = task.firstChild.textContent;
+            if (item.toLowerCase().indexOf(text) != -1) {
+                task.style.display =  'block';
+            } else {
+                task.style.display =  'none';
+            }
+        }
+    );
+}
